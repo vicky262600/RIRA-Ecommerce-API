b12b6cdda7a615957f3b6d4004a060bc34946435
vicky262600 <vickyjoshi2604@gmail.com>
2024-07-21 10:23:10 -0400

    vercel1

--- index.js
+++ index.js
@@ -17,7 +17,12 @@ mongoose.connect(
     .catch((err)=>{console.log(err);
 });
 
-app.use(cors());
+app.use(cors({
+    origin: ["https://redux-fe.vercel.app"],
+    methods: ["GET", "PUT", "POST", "DELETE"],
+    credentials: true
+}));
+
 app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/users", userRouter);
